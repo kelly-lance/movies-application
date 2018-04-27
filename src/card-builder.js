0586b7cfebca0732eb7619fc81078effe1ac4647
Kelly Villalobos <kvdesign1978@gmail.com>
2018-04-27 16:36:27 -0500

    pushing to lance for presentation

--- src/card-builder.js
+++ src/card-builder.js
@@ -2,7 +2,7 @@
 //------------module for creating cards---------------->
 export default (title, rating, id) => `<div class="col-lg-4 mt-4"><div class=card><div class="thumbnail"><a id="${id}" class="close mr-2 mt-1" href="#">×</a>
     <div class=card-body>
-         <h2>${title}</h2>
+         <h4>${title}</h4>
           <div class="rateYo" data-rating="${rating}"></div>
           <a class="float-right mb-3 editMe" href="#" data-id="${id}">Edit Movie</a>
     </div></div>`;
