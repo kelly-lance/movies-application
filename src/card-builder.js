7a738d35376c572ec17e08d091fcd04034d13eb9
Kelly Villalobos <kvdesign1978@gmail.com>
2018-04-27 12:33:24 -0500

    this looks like the final push

--- src/card-builder.js
+++ src/card-builder.js
@@ -5,6 +5,11 @@ export default (title, rating, id) => `<div class="col-lg-4 mt-4"><div class=car
     <div class=card-body>
          <h2>${title}</h2>
           <div class="rateYo" data-rating="${rating}"></div>
-          <a class="float-right mb-3" href="#" id="editMe">Edit Movie</a>
+          <a class="float-right mb-3 editMe" href="#" data-id="${id}">Edit Movie</a>
     </div></div>`;
 
+
+
+// check for duplicate IDs, convert to class
+// wire the modal close/hide to the edit button on the modal
+// ensure we're editing the right thing
